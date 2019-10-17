ae4beccfa49043a1289c467c7e4761fd09bb001d
HimriZngz <yingqi1561@qq.com>
2019-10-17 19:33:42 +0800

    二次提交

--- Z_practice/005.py
+++ Z_practice/005.py
@@ -1,5 +1,11 @@
 # 实现一个整数加法计算器
 # 思路： 用户输入 123+456 即以+为分离符号
 
-user_input = input('计算>>>')
+user_input = input('加法运算>>>')
 i = 0
+# print(user_input.split('+'))
+# 分离
+split_input = user_input.split('+')
+for x in split_input:
+    i += int(x)   # 如果没有int 就会报错，字符串不能与数字相加减
+print(i)
